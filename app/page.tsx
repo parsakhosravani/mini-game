8c90fc52d4e272839b56d23df5f20b3166d02add
parsakhosravani <parsakhosravani@gmail.com>
2024-06-04 20:09:44 +0330

    🔥 remove redirect link

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,6 @@
-"use client";
 import Image from "next/image";
 
 export default function Home() {
-  const redirect = () => {
-    setTimeout(() => {
-      window.location.assign("https://parsakhosravani.vercel.app/");
-    }, 1500);
-  };
   return (
     <main className="min-h-screen relative">
       <Image
@@ -19,7 +13,6 @@ export default function Home() {
         popoverTarget="mini-game-popover"
         className="absolute w-80 h-56"
         style={{ bottom: "20%", left: "50%", transform: "translateX(-50%)" }}
-        onClick={redirect}
       />
       <div
         className="bg-[#fd3c96] w-1/6 h-1/4 blur-3xl animate-pulse absolute"
